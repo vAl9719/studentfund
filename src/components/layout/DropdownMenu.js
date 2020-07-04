baed7db3255c5b787e5d1d49162014a52a2747c2
Alyssa Z <60488436+vAl9719@users.noreply.github.com>
2020-07-04 19:09:16 -0400

    Update DropdownMenu.js

--- src/components/layout/DropdownMenu.js
+++ src/components/layout/DropdownMenu.js
@@ -1,33 +1,55 @@
 import React from "react";
-import { Menu, Dropdown, Button } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
+import { Select } from 'antd';
+import axios from "axios";
 
+
+const {Option} = Select;
 class DropdownMenu extends React.Component {
-    renderMenu() {
-        return(
-            <Menu>
-                <Menu.Item key="0">
-                1st menu item
-                </Menu.Item>
-                <Menu.Item key="1">
-                2nd menu item
-                </Menu.Item>
-                <Menu.Item key="2">
-                3rd menu item
-                </Menu.Item>
-            </Menu>
-        );
+
+    handleChange = (value) => {
+        console.log(value);
+        axios({
+    "method":"GET",
+    "url":"https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-histories",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-rapidapi-host":"apidojo-yahoo-finance-v1.p.rapidapi.com",
+    "x-rapidapi-key":"b3cc80298bmshc51561cd78be00bp18a307jsn7343d212e7ad",
+    "useQueryString":true
+    },"params":{
+    "region":"US",
+    "lang":"en",
+    "symbol":value.key,
+    "from":"1231866000",
+    "to":"1547524844",
+    "events":"div",
+    "interval":"1mo",
+    }
+    })
+    .then((response)=>{
+      console.log(response);
+      console.log(response.data.chart.result[0].indicators.quote[0].close);
+
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+
+        
     }
     
     render(){
     return(
-    <Dropdown overlay={this.renderMenu()} trigger={["click"]}>
-      <Button>
-        <span>
-        <div className="title">{this.props.title} <DownOutlined /></div></span>
-      </Button>
-    </Dropdown>
-        );
+    <Select
+    labelInValue
+    defaultValue={{ key: 'AMZN' }}
+    style={{ width: 120 }}
+    onChange={this.handleChange}
+  >
+    <Option value="AAPL">Apple</Option>
+    <Option value="AMZN">Amazon</Option>
+  </Select>
+  );
     }
 }
 
