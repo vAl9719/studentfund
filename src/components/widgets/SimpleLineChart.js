ab92f918b04c186a7de60cd0acd46a7b7fae30d1
Alyssa Z <60488436+vAl9719@users.noreply.github.com>
2020-07-04 16:18:24 -0400

    Update SimpleLineChart.js

--- src/components/widgets/SimpleLineChart.js
+++ src/components/widgets/SimpleLineChart.js
@@ -1,52 +1,56 @@
 import React, { PureComponent } from 'react';
 import {
-  PieChart, Pie, Sector, Cell,
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 
+
 const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
+  {
+    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+  },
+  {
+    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+  },
+  {
+    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+  },
+  {
+    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+  },
+  {
+    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+  },
+  {
+    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+  },
+  {
+    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+  },
 ];
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx, cy, midAngle, innerRadius, outerRadius, percent, index,
-}) => {
-   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
-};
-
-export default class SimplePieChart extends PureComponent {
+class SimpleLineChart extends PureComponent {
 
   render() {
     return (
-      <PieChart width={400} height={400}>
-        <Pie
-          data={this.props.sectors || data}
-          cx={200}
-          cy={200}
-          labelLine={false}
-          label={renderCustomizedLabel}
-          outerRadius={105}
-          fill="#8884d8"
-          dataKey="value"
-        >
-          {
-            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-          }
-        </Pie>
-      </PieChart>
+      <LineChart
+        width={500}
+        height={300}
+        data={this.props.data || data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+      
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey={this.props.x || "name"} stroke="#FFFFFF"/>
+        <YAxis stroke="#FFFFFF"/>
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey={this.props.benchmark || this.props.sectorone || "pv"} stroke="#E11A2C" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey={this.props.portfolio || this.props.sectortwo || "uv"} stroke="#FFFFFF" />
+      </LineChart>
     );
   }
 }
+
+export default SimpleLineChart;
