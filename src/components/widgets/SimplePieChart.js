bce31a31696627c2f981c5f2a26b284094aac06a
Alyssa Z <60488436+vAl9719@users.noreply.github.com>
2020-07-04 16:18:05 -0400

    Update SimplePieChart.js

--- src/components/widgets/SimplePieChart.js
+++ src/components/widgets/SimplePieChart.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import {
-  PieChart, Pie, Legend, Tooltip,
+  PieChart, Pie, Sector, Cell,
 } from 'recharts';
 
-const data01 = [
-  { name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 },
-  { name: 'Group E', value: 278 }, { name: 'Group F', value: 189 },
+const data = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
 ];
 
-const data02 = [
-  { name: 'Group A', value: 2400 }, { name: 'Group B', value: 4567 },
-  { name: 'Group C', value: 1398 }, { name: 'Group D', value: 9800 },
-  { name: 'Group E', value: 3908 }, { name: 'Group F', value: 4800 },
-];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-export default class SimplePieChart extends React.Component{
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx, cy, midAngle, innerRadius, outerRadius, percent, index,
+}) => {
+   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+export default class SimplePieChart extends PureComponent {
 
   render() {
     return (
       <PieChart width={400} height={400}>
-        <Pie dataKey="value" isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={95} fill="#E11A2C" label />
-
-        <Tooltip />
+        <Pie
+          data={this.props.sectors || data}
+          cx={200}
+          cy={200}
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={105}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {
+            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+          }
+        </Pie>
       </PieChart>
     );
   }
