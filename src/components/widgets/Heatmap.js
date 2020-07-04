4f0d665f3cdd99b5fbf6db430041d03141b92097
Alyssa Z <60488436+vAl9719@users.noreply.github.com>
2020-07-04 15:59:44 -0400

    Heatmap but it's a Treemap

--- src/components/widgets/Heatmap.js
+++ src/components/widgets/Heatmap.js
@@ -1,49 +1,35 @@
-import React, {useEffect} from "react";
-import h337 from "heatmap.js";
+import React, { PureComponent } from 'react';
+import { Treemap } from 'recharts';
 
-const Heatmap = () => {
-
-  useEffect(() => {
-    var heatmapInstance = h337.create({
-      // only container is required, the rest will be defaults
-      container: document.querySelector('.App')
-    });
-    // now generate some random data
-    var points = [];
-    var max = 0;
-    var width = 500;
-    var height = 450;
-    var len = 450;
-
-    while (len--) {
-     var val = Math.floor(Math.random()*100);
-     max = Math.max(max, val);
-     var point = {
-      x: Math.floor(Math.random()*width),
-      y: Math.floor(Math.random()*height),
-      value: val
-     };
-     points.push(point);
-   }
-   // heatmap data format
-  var data = {
-    max: max,
-    data: points
-  };
-  // if you have a set of datapoints always use setData instead of addData
-  // for data initialization
-    heatmapInstance.setData(data);
- })
+const data = [
+          
+          {
+            name: 'data',
+            children: [
+              { name: 'Data', size: 20544 },
+              { name: 'DataList', size: 20000 },
+              { name: 'DataSprite', size: 15349 },
+              { name: 'NodeSprite', size: 19382 },
+        ]
+        }
+        ];
 
 
+class Heatmap extends PureComponent {
 
-  return (
-    <div className="App" style={{color:"black"}}>
-      ""
-    </div>
-  );
+  render() {
+    return (
+      <Treemap
+        width={400}
+        height={200}
+        data={this.props.holdings || data}
+        dataKey="size"
+        ratio={4 / 3}
+        stroke="#fff"
+        fill="#8884d8"
+      />
+    );
+  }
 }
 
 export default Heatmap;
-    
-
