b37085d5c273c93f73f4a732ed6aa84f55026643
Alyssa Z <60488436+vAl9719@users.noreply.github.com>
2020-07-05 12:31:53 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,6 @@
 import React from "react";
 import LoginPage from "./pages/LoginPage";
-import NavBar from "./layout/Navbar";
-import SimpleLineChart from "./widgets/SimpleLineChart";
-import SimplePieChart from "./widgets/SimplePieChart";
-import SimpleBarChart from "./widgets/SimpleBarChart";
-import DropdownMenu from "./layout/DropdownMenu";
-import Heatmap from "./widgets/Heatmap";
-import Card from "./widgets/Box";
-import InputForm from "./layout/InputForm";
-
-import "../App.css";
+import ContentPage from "./pages/ContentPage";
 
 class App extends React.Component {
     
@@ -17,15 +8,7 @@ class App extends React.Component {
         return (
         <div className="ui-container">
         <LoginPage/>
-        <div className="navbar"><NavBar/></div>
-        <div className="card"><Card/></div>
-        <div><InputForm/></div>
-        <div className="linechart-lower"><SimpleLineChart /></div>
-        <div className="dropdown-upper"><DropdownMenu/></div>
-        <div className="linechart-upper" style={{left:"750"}}><SimpleLineChart/></div>
-        <div className="piechart"><SimplePieChart/></div>
-        <div className="barchart"><SimpleBarChart/></div>
-        <div className="heatmap"><Heatmap/></div>
+        <ContentPage/>
         </div>
     );}
 };
