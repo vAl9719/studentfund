95c9e98c9fdb0253f2dfa21aa7fad13d39767787
Alyssa Z <60488436+vAl9719@users.noreply.github.com>
2020-07-05 12:23:03 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import LoginPage from "./pages/LoginPage";
 import NavBar from "./layout/Navbar";
 import SimpleLineChart from "./widgets/SimpleLineChart";
@@ -8,39 +7,27 @@ import SimpleBarChart from "./widgets/SimpleBarChart";
 import DropdownMenu from "./layout/DropdownMenu";
 import Heatmap from "./widgets/Heatmap";
 import Card from "./widgets/Box";
-import InputBox from "./layout/InputBox";
+import InputForm from "./layout/InputForm";
 
 import "../App.css";
 
-const App = () => {
-    return (
+class App extends React.Component {
+    
+    render(){
+        return (
         <div className="ui-container">
         <LoginPage/>
         <div className="navbar"><NavBar/></div>
         <div className="card"><Card/></div>
-        <div className="dropdown-lower"><DropdownMenu
-        title="Benchmark"/></div>
-        <div className="input-start-lower"><InputBox
-        entry="Start"/></div>
-        <div className="input-end-lower"><InputBox
-        entry="End"/></div>
-        <div className="times-lower"><DropdownMenu
-        title="Set Times"/></div>
-        <div className="linechart-lower"><SimpleLineChart/></div>
-        <div className="dropdown-upper"><DropdownMenu
-        title="Benchmark"/></div>
-        <div className="input-start-upper"><InputBox
-        entry="Start"/></div>
-        <div className="input-end-upper"><InputBox
-        entry="End"/></div>
-        <div className="times-upper"><DropdownMenu
-        title="Set Times"/></div>
+        <div><InputForm/></div>
+        <div className="linechart-lower"><SimpleLineChart /></div>
+        <div className="dropdown-upper"><DropdownMenu/></div>
         <div className="linechart-upper" style={{left:"750"}}><SimpleLineChart/></div>
         <div className="piechart"><SimplePieChart/></div>
         <div className="barchart"><SimpleBarChart/></div>
         <div className="heatmap"><Heatmap/></div>
         </div>
-    );
+    );}
 };
 
 export default App;
