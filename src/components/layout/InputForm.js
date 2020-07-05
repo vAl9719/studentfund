298fe7a64a0d78bdca6aa531ca2750b7ff0d272e
Alyssa Z <60488436+vAl9719@users.noreply.github.com>
2020-07-05 14:58:00 -0400

    Update InputForm.js

--- src/components/layout/InputForm.js
+++ src/components/layout/InputForm.js
@@ -18,6 +18,16 @@ class InputForm extends React.Component{
     onFinish = values => {
         console.log(values);
         console.log(values.benchmark);
+        console.log(values.start);
+        var startDate = new Date(values.start);
+        var endDate = new Date(values.end);
+        var msf = Math.floor(startDate.getTime()/1000.0);
+        var mst = Math.floor(endDate.getTime()/1000.0);
+        var from = msf.toString();
+        var to = mst.toString();
+        console.log(from);
+        console.log(to);
+
         axios({
     "method":"GET",
     "url":"https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-histories",
@@ -30,8 +40,8 @@ class InputForm extends React.Component{
     "region":"US",
     "lang":"en",
     "symbol":values.benchmark.value,
-    "from":"1231866000",
-    "to":"1547524844",
+    "from": from,
+    "to": to,
     "events":"div",
     "interval":values.interval.value,
     }
@@ -66,12 +76,12 @@ class InputForm extends React.Component{
             <div className="dropdown-lower"><Form.Item name="benchmark">
             <Select
                 labelInValue
-                defaultValue={{ key: 'SPX' }}
+                
                 style={{ width: 120 }}
                 
             >
                 <Option value="SPX">SP 500</Option>
-                <Option value="DJIA">Dow Jones</Option>
+                <Option value="DJI">Dow Jones</Option>
                 <Option value="NDAQ">Nasdaq</Option>
                 <Option value="RUT">Russell 2000</Option>
             </Select>
@@ -90,12 +100,13 @@ class InputForm extends React.Component{
             <div className="times-lower"><Form.Item name="interval">
             <Select
                 labelInValue
-                defaultValue={{ key: '1mo' }}
+                
                 style={{ width: 120 }}
             >
                 <Option value="1mo">1 Month</Option>
                 <Option value="3mo">3 Months</Option>
                 <Option value="1d">1 Day</Option>
+                <Option value="5d">5 Days</Option>
             </Select>
         </Form.Item></div>
         <Form.Item>
